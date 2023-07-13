9de35f7ccb56f423137857b051f9092ee1a3d5c4
aniitorosyann <137226340+aniitorosyann@users.noreply.github.com>
2023-07-13 05:31:12 -0700

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,17 @@
 from flask import Flask
 
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'our secret key'
 
     from .views import views
     from .auth import auth
+    from .data import data
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(data, url_prefix='/api')
 
     return app
