984ad90a150b9a8fd516ec1e876d533b6be97691
aniitorosyann <137226340+aniitorosyann@users.noreply.github.com>
2023-07-17 14:03:34 -0700

    add comments auth.py

--- website/auth.py
+++ website/auth.py
@@ -2,12 +2,14 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
+#route for logging in 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
 
+        #query the database for user 
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
@@ -19,12 +21,15 @@ def login():
         else:
             flash('Email does not exist.', category='error')
 
+    #render login template
     return render_template("login.html", user=current_user)
 
+#route for logging out
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
+#route for signing up
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
@@ -33,6 +38,7 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        #query the database for user 
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email already exists.', category='error')
@@ -47,10 +53,11 @@ def sign_up():
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(
                 password1, method='sha256'))
+            #add user to database
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
-
-    return render_template("sign_up.html", user=current_user)
+    #render sign up template
+    return render_template("sign_up.html", user="Ani")
