a580d97392e8d11d0fa7a7b10c183ab3a26a12d4
aniitorosyann <137226340+aniitorosyann@users.noreply.github.com>
2023-07-17 14:04:23 -0700

    add comments views.py

--- website/views.py
+++ website/views.py
@@ -1,7 +1,10 @@
+#import functions
 from flask import Blueprint, render_template
 
+#create blueprint
 views = Blueprint('views', __name__)
 
+#define route using blueprint
 @views.route('/')
 def home():
     return render_template("home.html")
