39600c46481eed2b8265eac65c2af69eeb5455d3
aniitorosyann <137226340+aniitorosyann@users.noreply.github.com>
2023-07-17 14:03:14 -0700

    add comments __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,17 +1,24 @@
 from flask import Flask
 
+#name database
 DB_NAME = "database.db"
 
+#define function to create flask application
 def create_app():
+    #create instance of Flask class
     app = Flask(__name__)
+    #create secret key
     app.config['SECRET_KEY'] = 'our secret key'
 
+    #import modules
     from .views import views
     from .auth import auth
     from .data import data
 
+    #create file blueprints
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(data, url_prefix='/api')
 
+    #return the app
     return app
