a16a6162f757af971d3cd95dfcfed012e7b9ca91
aniitorosyann <137226340+aniitorosyann@users.noreply.github.com>
2023-07-17 14:00:53 -0700

    add comments main1.py

--- main1.py
+++ main1.py
@@ -1,6 +1,9 @@
+#import from our website module
 from website import create_app
 
+#create instance and setup Flask
 app = create_app()
 
+#executes if script runs directly
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
